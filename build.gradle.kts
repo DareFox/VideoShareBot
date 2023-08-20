75c631a8e13a3efc1e60a91d1e7026d93ab4c0cd
DareFox <47672780+DareFox@users.noreply.github.com>
2023-08-20 03:18:49 +0300

    Gradle: add discord api library (kord-extensions)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,18 @@ plugins {
 group = "me.darefox"
 version = "1.0-SNAPSHOT"
 
+// Discord API version
+val kordExtVersion = "1.5.6"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    // Discord API
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:$kordExtVersion")
 }
 
 tasks.test {
