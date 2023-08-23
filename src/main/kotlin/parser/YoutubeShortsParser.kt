56d5f926fe8fb8340a7383159849838bf421a041
DareFox <47672780+DareFox@users.noreply.github.com>
2023-08-24 00:41:57 +0300

    YoutubeShortsParser: Use new wrapRegexPattern function

--- src/main/kotlin/parser/YoutubeShortsParser.kt
+++ src/main/kotlin/parser/YoutubeShortsParser.kt
@@ -3,9 +3,7 @@ package parser
 import enhancements.toValueList
 
 object YoutubeShortsParser : ServiceUrlParser("YoutubeShorts") {
-    private val regex = """${urlStartRegex}youtube\.com/shorts/\S{11}${urlEndMultilineRegex}""".toRegex(
-        setOf(RegexOption.MULTILINE)
-    )
+    private val regex = """youtube\.com/shorts/\S{11}""".wrapRegexPattern()
     override fun parse(text: String): List<String> {
         return regex.findAll(text).toValueList()
     }
