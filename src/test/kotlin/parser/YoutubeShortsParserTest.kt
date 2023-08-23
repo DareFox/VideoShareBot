fdb852851bcf9d258871a126e726c3ae7cf0b57f
DareFox <easymalink@gmail.com>
2023-08-24 00:30:13 +0300

    Test@YoutubeShortParser: add more valid and invalid urls

--- src/test/kotlin/parser/YoutubeShortsParserTest.kt
+++ src/test/kotlin/parser/YoutubeShortsParserTest.kt
@@ -6,11 +6,15 @@ import kotlin.test.assertEquals
 class YoutubeShortsParserTest: ParserTest(
     parser = YoutubeShortsParser,
     validUrls = listOf(
-        "youtube.com/shorts/abcdef12345"
+        "youtube.com/shorts/abcdef12345",
+        "www.youtube.com/shorts/abcdef12345",
+        "https://www.youtube.com/shorts/abcdef12345",
+        "https://youtube.com/shorts/abcdef12345"
     ),
     invalidUrls = listOf(
         "youtube.com/shorts/abc",
         "youtube.com",
-        "youtube.com/shorts/"
+        "youtube.com/shorts/",
+        "https://youtube.com/shorts/abcdef12345/"
     )
 )
