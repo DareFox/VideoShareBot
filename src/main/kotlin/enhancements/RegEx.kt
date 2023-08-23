eff38b0ddfc4187b3e60a4758dd85d462d4347dc
DareFox <easymalink@gmail.com>
2023-08-23 16:23:09 +0300

    Enhancements-RegEx: add List^<String^>.toRegex(..) function

--- src/main/kotlin/enhancements/RegEx.kt
+++ src/main/kotlin/enhancements/RegEx.kt
@@ -11,3 +11,43 @@ fun Sequence<MatchResult>.toValueList(): List<String> {
         it.value
     }
 }
+
+
+
+/**
+ * Extension function for converting a list of string patterns into a list of regular expressions with specified options.
+ *
+ * @receiver The list of string patterns to be converted.
+ * @param options The set of [RegexOption] to be applied to the regular expressions.
+ * @return A list of [Regex] objects corresponding to the converted string patterns with the specified options.
+ */
+fun List<String>.toRegex(options: Set<RegexOption>): List<Regex> {
+    return this.map {
+        it.toRegex(options)
+    }
+}
+
+/**
+ * Extension function for converting a list of string patterns into a list of regular expressions with a single option.
+ *
+ * @receiver The list of string patterns to be converted.
+ * @param option The single [RegexOption] to be applied to the regular expressions.
+ * @return A list of [Regex] objects corresponding to the converted string patterns with the specified option.
+ */
+fun List<String>.toRegex(option: RegexOption): List<Regex> {
+    return this.map {
+        it.toRegex(option)
+    }
+}
+
+/**
+ * Extension function for converting a list of string patterns into a list of regular expressions.
+ *
+ * @receiver The list of string patterns to be converted.
+ * @return A list of [Regex] objects corresponding to the converted string patterns.
+ */
+fun List<String>.toRegex(): List<Regex> {
+    return this.map {
+        it.toRegex()
+    }
+}
