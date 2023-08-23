2e8372ff57fd1e06f0b9807977cc69e18000b940
DareFox <easymalink@gmail.com>
2023-08-24 00:29:42 +0300

    YoutubeShortsParser: Use regex patterns of start and end of an url

--- src/main/kotlin/parser/YoutubeShortsParser.kt
+++ src/main/kotlin/parser/YoutubeShortsParser.kt
@@ -3,8 +3,8 @@ package parser
 import enhancements.toValueList
 
 object YoutubeShortsParser : ServiceUrlParser("YoutubeShorts") {
-    private val regex = """youtube\.com/shorts/\S{11}""".toRegex(
-        setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE)
+    private val regex = """${urlStartRegex}youtube\.com/shorts/\S{11}${urlEndMultilineRegex}""".toRegex(
+        setOf(RegexOption.MULTILINE)
     )
     override fun parse(text: String): List<String> {
         return regex.findAll(text).toValueList()
