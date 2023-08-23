7756d1206d671f8bf4566e4003a1d4fefb03528d
DareFox <47672780+DareFox@users.noreply.github.com>
2023-08-23 16:07:46 +0300

    YoutubeShortParser: use toValueList() on RegEx match

--- src/main/kotlin/parser/YoutubeShortsParser.kt
+++ src/main/kotlin/parser/YoutubeShortsParser.kt
@@ -1,13 +1,13 @@
 package parser
 
+import enhancements.toValueList
+
 object YoutubeShortsParser : ServiceUrlParser("YoutubeShorts") {
     private val regex = """youtube\.com/shorts/\S{11}""".toRegex(
         setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE)
     )
     override fun parse(text: String): List<String> {
-        return regex.findAll(text).toList().map {
-            it.value
-        }
+        return regex.findAll(text).toValueList()
     }
 }
 
