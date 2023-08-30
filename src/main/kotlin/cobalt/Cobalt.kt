1be512080868ee297e94ffa1102db8b09c03ac7d
DareFox <47672780+DareFox@users.noreply.github.com>
2023-08-30 23:18:35 +0300

    Cobalt: add singleton representing api

---
+++ src/main/kotlin/cobalt/Cobalt.kt
@@ -0,0 +1,15 @@
+package cobalt
+
+object Cobalt {
+    fun requestVideo(url: String) {
+        TODO()
+    }
+
+    fun requestAudio(url: String) {
+        TODO()
+    }
+
+    fun requestCustom(request: CobaltRequest): CobaltResponse {
+        TODO()
+    }
+}
