e49d5eef79ca2cb4fa6b51ba16e84a64453c44cf
DareFox <47672780+DareFox@users.noreply.github.com>
2023-08-30 23:21:33 +0300

    Cobalt: add audio field to CobaltResponse

--- src/main/kotlin/cobalt/CobaltResponse.kt
+++ src/main/kotlin/cobalt/CobaltResponse.kt
@@ -1,11 +1,16 @@
 package cobalt
 
+import kotlinx.serialization.Serializable
+import serialization.MixedNullableString
+
+@Serializable
 data class CobaltResponse(
     val status: CobaltResponseStatus,
     val url: String?,
     val text: String?,
     val pickerType: PickerType?,
     val picker: List<PickerItem>?,
-//    val audiom
+    @Serializable(with = MixedNullableString::class)
+    val audio: String?
 )
 
