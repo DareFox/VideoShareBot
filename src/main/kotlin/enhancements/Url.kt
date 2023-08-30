5b8d6451a99329d153a355f6a8352c66b40441ee
DareFox <easymalink@gmail.com>
2023-08-31 01:33:30 +0300

    Enchancements: add function to convert URL to filename

---
+++ src/main/kotlin/enhancements/Url.kt
@@ -0,0 +1,22 @@
+package enhancements
+
+import java.net.URL
+
+/**
+ * Extension function to clean and convert a URL into a safe filename format.
+ *
+ * This function takes a URL and processes its host and file components to generate
+ * a sanitized string that can be used as a filename.
+ *
+ * @receiver The URL to be cleaned and converted.
+ * @return A sanitized string suitable for use as a filename.
+ */
+fun URL.toSafeFilename(): String {
+    val regex = """\W+""".toRegex()
+    val underscoreRegex = """_{3,}""".toRegex()
+    val toConvert = host + file
+
+    return toConvert
+        .replace(regex, "_")
+        .replace(underscoreRegex, "__")
+}
