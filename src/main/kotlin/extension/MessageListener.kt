a854422c82c807daa6ea6a4f5e0cfc277c526477
DareFox <47672780+DareFox@users.noreply.github.com>
2023-08-31 00:08:13 +0300

    MessageListener: add minimal working code

--- src/main/kotlin/extension/MessageListener.kt
+++ src/main/kotlin/extension/MessageListener.kt
@@ -1,10 +1,28 @@
 package extension
 
+import cobalt.Cobalt
 import com.kotlindiscord.kord.extensions.events.EventContext
 import com.kotlindiscord.kord.extensions.extensions.event
+import com.kotlindiscord.kord.extensions.utils.respond
 import dev.kord.core.event.message.MessageCreateEvent
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.util.*
+import io.ktor.utils.io.jvm.javaio.*
+import ktor
+import parser.CompositeParser
+import parser.TikTokParser
+import parser.YoutubeShortsParser
+import java.io.File
+import java.io.InputStream
 
 class MessageListener : LoggerExtension("MessageListener") {
+    val parser = CompositeParser(
+        setOf(
+            TikTokParser,
+            YoutubeShortsParser
+        )
+    )
     override suspend fun setup() {
         log.info { "setup" }
         event<MessageCreateEvent> {
@@ -15,7 +33,17 @@ class MessageListener : LoggerExtension("MessageListener") {
         }
     }
 
+    @OptIn(InternalAPI::class)
     private suspend fun EventContext<MessageCreateEvent>.actionImpl() {
-        log.info { event.message.content }
+        if (event.member?.isBot == true) return
+        val shareUrl = parser.parse(event.message.content)
+
+        if (shareUrl.isEmpty()) return
+        val downloadUrl = Cobalt.requestVideo(shareUrl.first()) ?: return
+        val request = ktor.get(downloadUrl)
+
+        event.message.respond {
+            addFile("video.mp4", ChannelProvider { request.content })
+        }
     }
 }
