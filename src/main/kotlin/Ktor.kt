d9b159a279ed11590702c55a150ed26a055fd906
DareFox <47672780+DareFox@users.noreply.github.com>
2023-08-30 23:50:43 +0300

    Ktor; enable content negotiation

--- src/main/kotlin/Ktor.kt
+++ src/main/kotlin/Ktor.kt
@@ -1,4 +1,15 @@
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 
-val ktor = HttpClient(CIO)
+@OptIn(ExperimentalSerializationApi::class)
+val ktor = HttpClient(CIO) {
+    install(ContentNegotiation) {
+        json(Json {
+            explicitNulls = false
+        })
+    }
+}
