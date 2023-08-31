85cff8ed095cdc18c513bce6770e1e5d4b4c323b
DareFox <easymalink@gmail.com>
2023-08-31 13:03:36 +0300

    Parser: make serviceurl abstratc. not sealed

--- src/main/kotlin/parser/ServiceUrlParser.kt
+++ src/main/kotlin/parser/ServiceUrlParser.kt
@@ -5,7 +5,7 @@ package parser
  *
  * @property name A descriptive name for the specific online service.
  */
-sealed class ServiceUrlParser(val name: String) {
+abstract class ServiceUrlParser(val name: String) {
     /**
      * Regular expression pattern representing the start of a URL, including optional "https://" or "http://" and "www.".
      *
@@ -19,17 +19,27 @@ sealed class ServiceUrlParser(val name: String) {
      *
      * ### `(?![a-zA-Z1-9\/])\S*`
      */
-    protected val urlEndMultilineRegex = """(?![a-zA-Z1-9\/])\S*"""
+    protected val urlEndNoSlashMultiline = """(?![a-zA-Z1-9\/])\S*"""
+
+    /**
+     * Regular expression pattern representing the end of a URL where it should not be followed by letters, numbers.
+     * Url params are permitted and will be matched
+     *
+     * ### `(?![a-zA-Z1-9\/])\S*`
+     */
+    protected val urlEndWithSlashMultiline = """(?![a-zA-Z1-9\/])\S*"""
+
 
 
     /**
-     * Combines the [URL start pattern][urlStartRegex], a custom regex, and the [URL end pattern][urlEndMultilineRegex] to create a complete regex pattern.
+     * Combines the [URL start pattern][urlStartRegex], a custom regex, and the [URL end pattern][urlEndNoSlashMultiline] to create a complete regex pattern.
      * @param options A set of regex options to be applied to the resulting regex pattern. Defaults to [RegexOption.MULTILINE].
      * @return The combined regex pattern as a compiled regular expression.
      */
     protected fun String.wrapRegexPattern(
+        canEndOnSlash: Boolean = true,
         options: Set<RegexOption> = setOf(RegexOption.MULTILINE)
-    ) = "$urlStartRegex$this$urlEndMultilineRegex".toRegex(options)
+    ) = "$urlStartRegex$this$urlEndNoSlashMultiline".toRegex(options)
 
     /**
      * Parses the provided text and returns a list of URLs relevant to this specific online service.
