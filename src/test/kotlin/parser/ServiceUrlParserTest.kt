76e4c317a7ab9d59da62ca89dcdc4c33f78ff518
DareFox <47672780+DareFox@users.noreply.github.com>
2023-09-01 14:58:40 +0300

    Test: add more tests for serviceUrlParser

--- src/test/kotlin/parser/ServiceUrlParserTest.kt
+++ src/test/kotlin/parser/ServiceUrlParserTest.kt
@@ -9,13 +9,10 @@ class ServiceUrlParserTest: ServiceUrlParser("test") {
     }
 
     val baseUrlRegex = "example\\.com"
-    val possibleWwwUrlsRegex = "(www\\.|)$baseUrlRegex"
+    val wwwUrlRegex = "(www\\.|)$baseUrlRegex"
 
-    val startAndBaseUrlRegex = "$urlStartRegex$baseUrlRegex".toRegex()
-    val baseUrlEndNoSlashRegex = "$possibleWwwUrlsRegex$urlEndNoSlashMultiline".toRegex(RegexOption.MULTILINE)
-    val fullUrlNoSlashRegex = "$urlStartRegex$baseUrlRegex$urlEndNoSlashMultiline".toRegex(RegexOption.MULTILINE)
-    val baseUrlEndWithSlashRegex = "$possibleWwwUrlsRegex$urlEndWithSlashMultiline".toRegex(RegexOption.MULTILINE)
-    val fullUrlEndWithSlashRegex = "$urlStartRegex$baseUrlRegex$urlEndWithSlashMultiline".toRegex(RegexOption.MULTILINE)
+    val httpRegex = "$urlStartRegex$baseUrlRegex".toRegex()
+    val paramsWithNoSlashRegex = "$wwwUrlRegex$paramsWithoutSlash".toRegex(RegexOption.MULTILINE)
 
     val baseUrl = "example.com"
     val wwwUrl = "www.example.com"
@@ -23,8 +20,10 @@ class ServiceUrlParserTest: ServiceUrlParser("test") {
     val pathWithParams = "/test?param=value"
     val pathWithSlash = "/test/"
     val pathWithParamsAndSlash = "/test?param=value/"
-    val secondPathToIgnore = "/ignoreMe"
-    val secondPathToIgnoreWithSlash = "/ignoreMe/"
+    val ignorePath = "ignoreMe"
+    val ignorePathSlash = "$ignorePath/"
+    val ignorePathParams = "$ignorePath?param=value"
+    val ignorePathParamsAndSlash = "$ignorePath?param=value/"
 
     val baseUrls = listOf(
         baseUrl,
@@ -38,7 +37,7 @@ class ServiceUrlParserTest: ServiceUrlParser("test") {
         }
 
         rightUrls.forEach {
-            assertEquals(it, startAndBaseUrlRegex.find(it)!!.value)
+            assertEquals(it, httpRegex.find(it)!!.value)
         }
 
         val wrongUrls = listOf("htt://", "http:/", "http:///", "htts://", "https:/", "https:///").flatMap {
@@ -46,69 +45,119 @@ class ServiceUrlParserTest: ServiceUrlParser("test") {
         }
 
         wrongUrls.forEach {
-            val regexValue = startAndBaseUrlRegex.find(it)?.value
+            val regexValue = httpRegex.find(it)?.value
             assertContains(listOf(baseUrl, wwwUrl), regexValue)
         }
     }
 
+    // paramsWithoutSlash
+
     @Test
-    fun `urlEndNoSlashMultiline should not match with slash`() {
-        baseUrls.flatMap {
-            listOf(it + pathWithSlash, it + pathWithParamsAndSlash)
-        }.forEach {
-            assertNull(baseUrlEndNoSlashRegex.find(it))
+    fun `paramsWithoutSlash shouldn't match if slash at the end`() {
+        baseUrls.flatMap { listOf(it + pathWithSlash) }.forEach {
+            println(it)
+            assertNull(paramsWithNoSlashRegex.find(it))
         }
     }
 
     @Test
-    fun `urlEndNoSlashMultiline should match without slash`() {
-        baseUrls.map {
-            it + path
-        }.forEach {
+    fun `paramsWithoutSlash shouldn't match if next path exists`() {
+        baseUrls.flatMap { listOf(it + pathWithSlash + ignorePath) }.forEach {
             println(it)
-            val regex = "$possibleWwwUrlsRegex$path$urlEndNoSlashMultiline".toRegex()
-            val value = regex.find(it)?.value
-            assertNotNull(value)
-            assertEquals(it, value)
+            assertNull(paramsWithNoSlashRegex.find(it))
         }
     }
 
     @Test
-    fun `urlEndNoSlashMultiline should match params without slash`() {
-        baseUrls.map {
-            it + pathWithParams
-        }.forEach {
+    fun `paramsWithoutSlash shouldn't match if next path with slash exists`() {
+        baseUrls.flatMap { listOf(it + pathWithSlash + ignorePathSlash) }.forEach {
             println(it)
-            val regex = "$possibleWwwUrlsRegex$path$urlEndNoSlashMultiline".toRegex()
-            val value = regex.find(it)?.value
-            assertNotNull(value)
-            assertEquals(it, value)
+            assertNull(paramsWithNoSlashRegex.find(it))
         }
     }
 
     @Test
-    fun `urlEndWithSlashMultiline should match params with slash`() {
-        baseUrls.map {
-            it + pathWithSlash
-        }.forEach {
+    fun `paramsWithoutSlash should match path without slash`() {
+        shouldMatch("$wwwUrlRegex$path$paramsWithoutSlash".toRegex()) {
+            listOf(it + path)
+        }
+    }
+
+    @Test
+    fun `paramsWithoutSlash should match params without slash`() {
+        shouldMatch("$wwwUrlRegex$path$paramsWithoutSlash".toRegex()) {
+            listOf(it + pathWithParams)
+        }
+    }
+    @Test
+    fun `paramsWithoutSlash shouldn't match params with slash`() {
+        baseUrls.flatMap { listOf(it + pathWithParamsAndSlash) }.forEach {
             println(it)
-            val regex = "$possibleWwwUrlsRegex$path$urlEndNoSlashMultiline".toRegex()
-            val value = regex.find(it)?.value
-            assertNotNull(value)
-            assertEquals(it, value)
+            assertNull(paramsWithNoSlashRegex.find(it))
+        }
+    }
+
+    @Test
+    fun `paramsWithoutSlash shouldn't match if next path with params exists`() {
+        baseUrls.flatMap { listOf(it + pathWithSlash + ignorePathParams) }.forEach {
+            println(it)
+            assertNull(paramsWithNoSlashRegex.find(it))
+        }
+    }
+
+    @Test
+    fun `paramsWithoutSlash shouldn't match if next path with params and slash exists`() {
+        baseUrls.flatMap { listOf(it + pathWithSlash + ignorePathParamsAndSlash) }.forEach {
+            println(it)
+            assertNull(paramsWithNoSlashRegex.find(it))
+        }
+    }
+
+    //
+    // paramsWithPossibleSlash
+    //
+
+    @Test
+    fun `paramsWithPossibleSlash should match path with slash`() {
+        shouldMatch("$wwwUrlRegex$path$paramsWithPossibleSlash".toRegex()) {
+            listOf(it + pathWithSlash)
+        }
+    }
+
+    @Test
+    fun `paramsWithPossibleSlash should match path without slash`() {
+        shouldMatch("$wwwUrlRegex$path$paramsWithPossibleSlash".toRegex()) {
+            listOf(it + path)
+        }`
+    }
+
+    @Test
+    fun `paramsWithPossibleSlash should match params with slash`() {
+        shouldMatch("$wwwUrlRegex$path$paramsWithoutSlash".toRegex()) {
+            listOf(it + pathWithParamsAndSlash)
+        }
+    }
+
+    @Test
+    fun `paramsWithPossibleSlash should match params without slash`() {
+        shouldMatch("$wwwUrlRegex$path$paramsWithoutSlash".toRegex()) {
+            listOf(it + pathWithParams)
         }
     }
 
     private fun shouldMatch(regex: String, rightUrlBuilder: (String) -> List<String>) = shouldMatch(regex.toRegex(), rightUrlBuilder)
-
     private fun shouldMatch(regex: Regex, rightUrlBuilder: (String) -> List<String>) {
         baseUrls
             .flatMap(rightUrlBuilder)
             .forEach {
-                println(it)
+                printMatch(regex, it)
                 val value = regex.find(it)?.value
                 assertNotNull(value)
                 assertEquals(it, value)
             }
+    }
+
+    private fun printMatch(regex: Regex, toMatch: String) {
+        println("${regex.pattern}\t$toMatch")
     }
 }
