fee9c3ae33db33f7884c3a2574a7269f7d5f5e3f
DareFox <easymalink@gmail.com>
2023-09-11 12:47:17 +0300

    MessageListener: use MimeMap to caluclate extension

--- src/main/kotlin/extension/MessageListener.kt
+++ src/main/kotlin/extension/MessageListener.kt
@@ -1,21 +1,20 @@
 package extension
 
+import MimeMap
 import cobalt.Cobalt
 import com.kotlindiscord.kord.extensions.events.EventContext
 import com.kotlindiscord.kord.extensions.extensions.event
 import com.kotlindiscord.kord.extensions.utils.respond
+import com.sun.nio.sctp.IllegalReceiveException
 import dev.kord.core.event.message.MessageCreateEvent
 import enhancements.toSafeFilename
 import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.util.*
-import io.ktor.utils.io.jvm.javaio.*
 import ktor
 import parser.CompositeParser
 import parser.TikTokParser
 import parser.YoutubeShortsParser
-import java.io.File
-import java.io.InputStream
 import java.net.URL
 
 class MessageListener : LoggerExtension("MessageListener") {
@@ -26,7 +25,6 @@ class MessageListener : LoggerExtension("MessageListener") {
         )
     )
     override suspend fun setup() {
-        log.info { "setup" }
         event<MessageCreateEvent> {
             action {
                 actionImpl()
@@ -43,8 +41,12 @@ class MessageListener : LoggerExtension("MessageListener") {
         val downloadUrl = Cobalt.requestVideo(shareUrl.first()) ?: return
         val request = ktor.get(downloadUrl)
 
+        val contentType = request.headers.get("Content-Type") ?: throw IllegalReceiveException("No content-type")
+        val extension = MimeMap[contentType] ?: throw IllegalReceiveException("Content-type $contentType is not supported")
+        val filename = URL(shareUrl.first()).toSafeFilename() + extension
+
         event.message.respond {
-            addFile(URL(shareUrl.first()).toSafeFilename() + ".mp4", ChannelProvider { request.content })
+            addFile(filename, ChannelProvider { request.content })
         }
     }
 }
