da89110af0a5f82de69b43a448251ef9eed3749e
DareFox <easymalink@gmail.com>
2023-09-11 12:37:01 +0300

    TikTokParser: adapt to ServiceUrlParser changes

--- src/main/kotlin/parser/TikTokParser.kt
+++ src/main/kotlin/parser/TikTokParser.kt
@@ -1,16 +1,57 @@
 package parser
 
-import enhancements.findAll
-import enhancements.toRegex
-import enhancements.toValueList
+import enhancements.CheckMachine
+import enhancements.isOnlyTrue
 
 object TikTokParser : ServiceUrlParser("TikTok") {
-    private val regex = listOf(
-        """tiktok\.com/[^\s\/]+?/video/\S{19}""",
-        """vm\.tiktok\.com/.{9}"""
-    ).map { it.wrapRegexPattern() }
-
     override fun parse(text: String): List<String> {
-        return regex.findAll(text).toValueList()
+        val list = anyUrlRegex.findAll(text).mapNotNull {
+            urlCheck(it.value)
+        }.toList()
+
+        return list
+    }
+
+    private fun urlCheck(url: String): String? {
+        val splitter = UrlSplitter(url)
+        return when (splitter.host) {
+            "vm.tiktok.com" -> shortLinkCheck(splitter)
+            "tiktok.com" -> longLinkCheck(splitter)
+            else -> {
+                log.debug { "$url is invalid" }
+                null
+            }
+        }
+    }
+
+    private fun shortLinkCheck(splitter: UrlSplitter): String? {
+        val check = CheckMachine(
+            "isIdGood" to (splitter.pathSplit.getOrNull(0)?.length == 9),
+            "pathLength" to (splitter.pathSplit.size == 1),
+            "noSlash" to !splitter.endsOnSlash
+        )
+
+        log.debug { """
+            url = ${splitter.url},
+            checks = $check,
+            splitter = $splitter
+        """.trimIndent() }
+        return if (check.isAllTrue) splitter.url else null
+    }
+
+    private fun longLinkCheck(splitter: UrlSplitter): String? {
+        val check = CheckMachine(
+            "isVideo" to (splitter.pathSplit.getOrNull(1) == "video"),
+            "isIdGood" to (splitter.pathSplit.getOrNull(2)?.length == 19),
+            "pathLength" to (splitter.pathSplit.size == 3),
+            "noSlash" to !splitter.endsOnSlash
+        )
+
+        log.debug { """
+            url = ${splitter.url},
+            checks = $check,
+            splitter = $splitter
+        """.trimIndent() }
+        return if (check.isAllTrue) splitter.url else null
     }
 }
