78c973a95be1d2999ccc36cbbdcef26352dc71e8
DareFox <47672780+DareFox@users.noreply.github.com>
2024-01-02 17:26:56 +0300

    tooling: add function to send error as embed

---
+++ src/main/kotlin/enhancements/Message.kt
@@ -0,0 +1,16 @@
+package enhancements
+
+import com.kotlindiscord.kord.extensions.utils.respond
+import dev.kord.core.entity.Message
+import dev.kord.rest.builder.message.create.embed
+
+suspend fun Message.sendErrorAsEmbed(err: Throwable) {
+    respond {
+        embed {
+            title = "ERROR"
+            field("Class", true) {"`${err.javaClass.name}`"}
+            field("Message", true) {"`${err.message.toString()}`"}
+            field("Callstack", false) { "```kotlin\n${err.stackTraceToString()}```" }
+        }
+    }
+}
