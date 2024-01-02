6a3fd18312b86f3f82edea4084368806fb55aaec
DareFox <easymalink@gmail.com>
2024-01-02 17:26:20 +0300

    fix: add www subdomain to youtube

--- src/main/kotlin/match/YoutubeShortsMatcher.kt
+++ src/main/kotlin/match/YoutubeShortsMatcher.kt
@@ -5,6 +5,7 @@ import enhancements.nestedListOf
 object YoutubeShortsMatcher : UrlMatcher() {
     override val pattern = UrlPattern(
         baseDomains = listOf("youtube.com"),
+        subdomains = listOf("", "www"),
         segmentMatchers = nestedListOf(
             SpecificText(shouldBe = "shorts"),
             Text(length = 11)
