0eb5185ed84522f45ef180b2b628b7ea83aa81bd
DareFox <easymalink@gmail.com>
2024-01-02 21:03:41 +0300

    tooling: add CombinedMatcher and TextStartsWith
    CombinedMatcher: Combines multiple matchers into a one
    TextStartsWith: validates if text starts with specified prefix

--- src/main/kotlin/match/UrlSegmentMatcher.kt
+++ src/main/kotlin/match/UrlSegmentMatcher.kt
@@ -20,9 +20,20 @@ object Anything: UrlSegmentMatcher {
     override fun validate(text: String): Boolean = true
 }
 
+class TextStartsWith(val prefix: String, val ignoreCase: Boolean): UrlSegmentMatcher {
+    override fun validate(text: String): Boolean = text.startsWith(prefix, ignoreCase)
+}
+
 class Number(val length: Int): UrlSegmentMatcher {
     override fun validate(text: String): Boolean {
         text.toLongOrNull() ?: return false
         return text.length == length
+    }
+}
+
+class CombinedMatcher(vararg val matchers: UrlSegmentMatcher): UrlSegmentMatcher {
+    override fun validate(text: String): Boolean {
+        matchers.firstOrNull { it.validate(text) } ?: return false
+        return true
     }
 }
