4eeb66ef837e6e3e1ecf4fd12f874ce1d836b15d
DareFox <47672780+DareFox@users.noreply.github.com>
2024-01-03 08:52:00 +0300

    refactor: return url with parser that parsed it in CompositeMatcher

--- src/main/kotlin/match/CompositeMatcher.kt
+++ src/main/kotlin/match/CompositeMatcher.kt
@@ -1,25 +1,50 @@
 package match
 
 /**
- * A composite matcher that aggregates and utilizes a set of service-specific matchers to parse URLs from a given text.
+ * A composite matcher that aggregates multiple service-specific matchers to parse URLs from a given text.
  *
- * @property parsers A set of service-specific matcher to be used for parsing.
+ *
+ * @constructor Creates a [CompositeMatcher] with a set of provided [parsers].
+ * @param parsers A set of [UrlMatcher] instances responsible for parsing URLs from text.
+ *
+ * @see UrlMatcher
+ * @see CompositeMatcherResult
  */
 class CompositeMatcher(private val parsers: Set<UrlMatcher>) {
 
     /**
-     * Parses the provided text using each service-specific parser and returns a list of URLs.
+     * Parses the given text using each of the aggregated [parsers] and returns a consolidated list of URLs.
+     *
+     * This method orchestrates the parsing process by delegating to each individual parser and collecting
+     * the results into a single list. It ensures that URLs parsed by any of the service-specific matchers
+     * are included in the final output.
      *
      * @param string The text to be parsed for URLs.
-     * @return A list of URLs parsed from the text using the aggregated matchers.
+     * @return A list of [CompositeMatcherResult] objects, each representing a parsed URL and the parser that identified it.
      */
-    fun parse(string: String): List<String> {
-        val urlList = mutableListOf<String>()
+    fun parse(string: String): List<CompositeMatcherResult> {
+        val urlList = mutableListOf<CompositeMatcherResult>()
 
         for (parser in parsers) {
-            urlList += parser.parse(string)
+            urlList += parser.parse(string).map {
+                CompositeMatcherResult(it, parser)
+            }
         }
 
         return urlList
     }
 }
+
+/**
+ * Represents a parsed URL along with the specific [UrlMatcher] that parsed it.
+ *
+ * This data class encapsulates the parsed URL and its associated parser, providing context about
+ * the source of the parsed URL within a composite parsing scenario.
+ *
+ * @property url The parsed URL.
+ * @property parser The [UrlMatcher] that parsed this URL.
+ */
+data class CompositeMatcherResult(
+    val url: String,
+    val parser: UrlMatcher
+)
