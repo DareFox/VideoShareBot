ff8dfc6278cf1bf8806605b97bab8c56c672f1e8
DareFox <47672780+DareFox@users.noreply.github.com>
2024-01-03 08:45:56 +0300

    docs: add KDoc for UrlMatcher and UrlSegmentMatcher

--- src/main/kotlin/match/UrlSegmentMatcher.kt
+++ src/main/kotlin/match/UrlSegmentMatcher.kt
@@ -1,9 +1,24 @@
 package match
 
+/**
+ * A functional interface defining the behavior of validating path element within a URL.
+ *
+ * ## What is path element?
+ *
+ * Example link: `https://google.com/skyfall/when/you/crumble`
+ *
+ * Path element in link is `skyfall` ; `when` ; `you` ; `crumble`
+ */
 interface UrlSegmentMatcher {
+
+    /**
+     * Examines the provided text and determines whether it aligns with the specified validation criteria.
+     *
+     * @param text The text of the URL segment to be validated.
+     * @return `true` if the text matches the criteria, `false` otherwise.
+     */
     fun validate(text: String): Boolean
 }
-
 class SpecificText(val shouldBe: String, val ignoreCase: Boolean = false): UrlSegmentMatcher {
     override fun validate(text: String): Boolean = text.equals(shouldBe, false)
 }
