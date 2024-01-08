63fb45f46abdefe29280e44c5ed218597ab3b439
DareFox <47672780+DareFox@users.noreply.github.com>
2024-01-08 17:09:35 +0300

    tooling: add infix operator OR for UrlSegmentMatcher

---
+++ src/main/kotlin/match/Operators.kt
@@ -0,0 +1,7 @@
+package match
+
+infix fun UrlSegmentMatcher.or(matcher: UrlSegmentMatcher): UrlSegmentMatcher {
+    return UrlSegmentMatcher {
+        this.validate(it) || matcher.validate(it)
+    }
+}
