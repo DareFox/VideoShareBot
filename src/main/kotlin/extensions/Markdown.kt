c79408010553266f8f9e326b95b7990cd4bdf909
DareFox <47672780+DareFox@users.noreply.github.com>
2024-01-08 17:07:43 +0300

    tooling: add extension to convert text to markdown

---
+++ src/main/kotlin/extensions/Markdown.kt
@@ -0,0 +1,10 @@
+package extensions
+
+fun String.toSingleCodeLineMarkdown(): String {
+    return "`$this`"
+}
+
+fun String.toCodeMarkdown(lang: String? = null): String {
+    val langString = lang ?: ""
+    return "```$langString\n$this\n```"
+}
