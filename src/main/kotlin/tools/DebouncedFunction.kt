1bd698058dfc6c1eaf39a92f0a0edb914d1d6b81
DareFox <easymalink@gmail.com>
2024-01-09 10:43:49 +0300

    tooling: add class that debounces function with specified delay
    Example Usage: prevent hitting rate limit for editing message, function will use last value given and send it

---
+++ src/main/kotlin/tools/DebouncedFunction.kt
@@ -0,0 +1,45 @@
+package tools
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlin.time.Duration
+
+class DebouncedFunction<T>(private val delayDuration: Duration, private val function: (T) -> Unit) {
+    private val scope = CoroutineScope(context = Dispatchers.Default)
+    private val mutex = Mutex()
+
+    private var lastValue: T? = null
+        set(value) {
+            field = value
+            assigned = true
+        }
+
+    private var assigned = false
+    private var delayedJob: Job? = null
+
+    @Suppress("UNCHECKED_CAST")
+    private fun launchDelayJob() {
+        val mutexLocked = mutex.tryLock()
+        try {
+            if (mutexLocked && delayedJob?.isCompleted != false && assigned) {
+                delayedJob = scope.launch {
+                    val value = lastValue as T
+                    callFunction(value)
+                    delay(delayDuration)
+                }
+            }
+        } finally {
+            if (mutexLocked) mutex.unlock()
+        }
+
+    }
+
+    private fun callFunction(arg: T) {
+        function(arg)
+    }
+
+    fun run(arg: T) {
+        lastValue = arg
+        launchDelayJob()
+    }
+}
