dc398bb1e22013c90d3fd177e33be0472fc948a6
DareFox <easymalink@gmail.com>
2024-01-20 09:59:28 +0300

    logs(sendErrorAsEmbed): log error

--- src/main/kotlin/extensions/Message.kt
+++ src/main/kotlin/extensions/Message.kt
@@ -2,7 +2,9 @@ package extensions
 
 import com.kotlindiscord.kord.extensions.utils.respond
 import dev.kord.core.entity.Message
+import dev.kord.core.kordLogger
 import dev.kord.rest.builder.message.create.embed
+import io.ktor.util.logging.*
 
 suspend fun Message.sendErrorAsEmbed(err: Throwable) {
     respond {
@@ -11,6 +13,7 @@ suspend fun Message.sendErrorAsEmbed(err: Throwable) {
             field("Class", true) {"`${err.javaClass.name}`"}
             field("Message", true) {"`${err.message.toString()}`"}
             field("Callstack", false) { "```kotlin\n${err.stackTraceToString()}```" }
+            kordLogger.error(err)
         }
     }
 }
