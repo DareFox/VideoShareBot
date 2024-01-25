f8dea3b38c11486d7b43a66ef1f32c31814b4d36
DareFox <easymalink@gmail.com>
2024-01-25 15:18:45 +0300

    fix(pow): remove one wrong additional multiplication

--- src/main/kotlin/me/darefox/videosharebot/extensions/Math.kt
+++ src/main/kotlin/me/darefox/videosharebot/extensions/Math.kt
@@ -2,7 +2,7 @@ package me.darefox.videosharebot.extensions
 
 fun pow(num: Int, exp: Int): Long {
     var result: Long = num.toLong()
-    repeat(exp) {
+    repeat(exp - 1) {
         result *= num
     }
 
