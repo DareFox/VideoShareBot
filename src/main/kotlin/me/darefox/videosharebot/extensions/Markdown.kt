55a89c2756deb0b8cb1321a29d3490ad0b2fcccc
DareFox <47672780+DareFox@users.noreply.github.com>
2024-01-25 14:49:25 +0300

    refactor: rename markdown extens. functions and use arguments instead of this

--- src/main/kotlin/me/darefox/videosharebot/extensions/Markdown.kt
+++ src/main/kotlin/me/darefox/videosharebot/extensions/Markdown.kt
@@ -1,10 +1,10 @@
 package me.darefox.videosharebot.extensions
 
-fun String.toSingleCodeLineMarkdown(): String {
-    return "`$this`"
+fun asInlineCode(code: String): String {
+    return "`$code`"
 }
 
-fun String.toCodeMarkdown(lang: String? = null): String {
+fun asCodeBlock(code: String, lang: String? = null): String {
     val langString = lang ?: ""
-    return "```$langString\n$this\n```"
+    return "```$langString\n${code.trim()}\n```"
 }
