a5814f308501f6834869eb479963d052d6dd31fc
DareFox <47672780+DareFox@users.noreply.github.com>
2024-01-30 16:20:28 +0300

    refactor(DebouncedFunction): rename and rewrite
    - Rename DebouncedFunction to ThrottledFunction, because throttling doesn't reset timer between calls
    - Use Flow instead of Mutex
    - Use function supertype for classes
    - Don't create CoroutineScope because it makes jobs uncancellable, use scope provided by caller.
    - Add top level CoroutineScope extensions for creating throttle functions

---
+++ src/main/kotlin/me/darefox/videosharebot/tools/ThrottledFunction.kt
@@ -0,0 +1,119 @@
+package me.darefox.videosharebot.tools
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlin.time.Duration
+
+/**
+ * Throttles the invocation of a suspend function with a single argument [T].
+ *
+ * This class ensures that the provided [function] is not called more frequently
+ * than the specified [delayDuration]. It can optionally filter out duplicate
+ * arguments based on the [uniqueArguments] flag.
+ *
+ * @param scope The [CoroutineScope] to launch the throttling coroutine.
+ * @param delayDuration The duration to delay between invocations of [function].
+ * @param uniqueArguments Flag indicating whether to filter out duplicate arguments.
+ * @param function The suspend function to be throttled.
+ *
+ * @param T The type of the argument passed to the throttled function.
+ */
+class ThrottledArgumentFunction<T>(
+    scope: CoroutineScope,
+    private val delayDuration: Duration,
+    private val uniqueArguments: Boolean,
+    private val function: suspend (T) -> Unit,
+) : (T) -> Unit {
+    private val sharedFlow: MutableSharedFlow<T> =
+        MutableSharedFlow(replay = 1, onBufferOverflow = BufferOverflow.DROP_LATEST)
+    private val collectFlow = if (uniqueArguments) {
+        sharedFlow.distinctUntilChanged()
+    } else {
+        sharedFlow
+    }
+
+    private val job = scope.launch(CoroutineName("ThrottledArgumentFunction-Loop")) { jobLoop() }
+    private suspend fun jobLoop() {
+        while (true) {
+            collectFlow.collect {
+                function(it)
+                delay(delayDuration)
+            }
+        }
+    }
+
+    override fun invoke(p1: T) {
+        sharedFlow.tryEmit(p1)
+    }
+}
+
+
+/**
+ * Throttles the invocation of a suspend function with no arguments.
+ *
+ * This class ensures that the provided [function] is not called more frequently
+ * than the specified [delayDuration].
+ *
+ * @param scope The [CoroutineScope] to launch the throttling coroutine.
+ * @param delayDuration The duration to delay between invocations of [function].
+ * @param function The suspend function to be throttled.
+ */
+class ThrottledFunction(
+    private val scope: CoroutineScope,
+    private val delayDuration: Duration,
+    private val function: suspend () -> Unit
+) : () -> Unit {
+    @Volatile
+    private var counter: Int = 0
+
+    private val job = scope.launch(CoroutineName("ThrottledFunction")) {
+        var previous = counter
+        while (true) {
+            val now = counter
+            if (now == previous) {
+                yield()
+            } else {
+                function()
+                delay(delayDuration)
+            }
+            previous = now
+        }
+    }
+
+    override fun invoke() {
+        counter++
+    }
+}
+
+/**
+ * Creates a throttled version of a suspend function with a single argument [T].
+ *
+ * @param delayDuration The duration to delay between successive invocations of the function.
+ * @param uniqueArguments Flag indicating whether to filter out duplicate arguments.
+ * @param function The suspend function to be throttled.
+ *
+ * @param T The type of the argument passed to the throttled function.
+ *
+ * @return A throttled version of the provided suspend function.
+ */
+fun <T> CoroutineScope.throttleFuncArg(
+    delayDuration: Duration,
+    uniqueArguments: Boolean,
+    function: suspend (T) -> Unit
+): (T) -> Unit {
+    return ThrottledArgumentFunction(this, delayDuration, uniqueArguments, function)
+}
+
+/**
+ * Creates a throttled version of a suspend function with no arguments.
+ *
+ * @param delayDuration The duration to delay between successive invocations of the function.
+ * @param function The suspend function to be throttled.
+ *
+ * @return A throttled version of the provided suspend function.
+ */
+fun CoroutineScope.throttleFunc(delayDuration: Duration, function: suspend () -> Unit): () -> Unit {
+    return ThrottledFunction(this, delayDuration, function)
+}
