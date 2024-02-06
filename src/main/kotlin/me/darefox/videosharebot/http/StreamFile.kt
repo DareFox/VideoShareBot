47f1f952043027df4ec9c4c9f7dbb000162006aa
DareFox <easymalink@gmail.com>
2024-02-06 14:32:02 +0300

    refactor(http): replace http4k with Ktor
    I know it looks silly. First time, I replaced ktor with http4k, and now I'm replacing http4k with ktor
    
    Why? Http4k has no support for coroutine cancellation, if coroutine cancells, request still will be processed
    Also, its actually provides a lot of useful functions that replaces my bad written implementations (MimeMap/Streaming processing)

--- src/main/kotlin/me/darefox/videosharebot/http/StreamFile.kt
+++ src/main/kotlin/me/darefox/videosharebot/http/StreamFile.kt
@@ -1,65 +1,8 @@
 package me.darefox.videosharebot.http
 
-import dev.forkhandles.result4k.Failure
-import dev.forkhandles.result4k.Success
-import me.darefox.videosharebot.extensions.ResultMonad
-import me.darefox.videosharebot.extensions.fold
-import me.darefox.videosharebot.extensions.toSafeFilename
-import me.darefox.videosharebot.extensions.tryAsResult
-import org.apache.commons.io.input.CountingInputStream
-import org.http4k.core.Method
-import org.http4k.core.Request
-import org.http4k.core.Response
-import java.io.IOException
-import java.net.URL
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 
-data class StreamFile(
-    val response: Response,
-    val filename: String,
-    val size: Long?,
-    val stream: CountingInputStream
-)
-
-fun streamInternetFile(getUrl: String): ResultMonad<StreamFile, Exception> =
-    streamInternetFile(Request(Method.GET, getUrl))
-
-fun streamInternetFile(request: Request): ResultMonad<StreamFile, Exception> {
-    val response = tryAsResult<Response, IOException> { HttpStreamingClient(request) }
-        .fold(
-            ifSuccess = { value },
-            ifFailure = { return this }
-        )
-
-    val contentDisposition = response.header("Content-Disposition")
-    val contentType = response.header("Content-Type")
-    val filename = when {
-        contentDisposition != null -> contentDespositionFilename(contentDisposition)
-        contentType != null -> contentTypeFilename(contentType, request.uri.toString())
-        else -> return Failure(IOException("No candidates for filename"))
-    }.fold(
-        ifSuccess = { value },
-        ifFailure = { return Failure(IOException(reason)) }
-    )
-
-
-    return Success(
-        StreamFile(
-            response = response,
-            filename = filename,
-            size = response.header("Content-Length")?.toLongOrNull(),
-            stream = CountingInputStream(response.body.stream)
-        )
-    )
-}
-
-private fun contentDespositionFilename(contentDisposition: String): ResultMonad<String, String> {
-    val regex = Regex("(?<=filename=\").*(?=\")")
-    val filename = regex.find(contentDisposition)?.value
-    return filename?.let { Success(it) } ?: Failure("Wrong Content-Disposition value")
-}
-
-private fun contentTypeFilename(contentType: String, url: String): ResultMonad<String, String> {
-    val extension = MimeMap[contentType] ?: return Failure("$contentType is not supported media")
-    val baseFilename = URL(url).toSafeFilename()
-    return Success(baseFilename + extension)
+suspend fun <T> requestFile(getUrl: String, streamProcess: suspend (HttpResponse) -> T): T {
+    return KtorHttp.prepareGet(getUrl).execute(streamProcess)
 }
