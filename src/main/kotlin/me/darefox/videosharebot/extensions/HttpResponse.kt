a7f5104b73f280e61500533a0646ad540c4864fd
DareFox <easymalink@gmail.com>
2024-02-06 15:27:22 +0300

    refactor(MimeMap): return MimeMap and use it for filename
    Ktor gives all extensions and some of them not supported in Discord, even if they are the same (.jfif/.jpg)
    
    So now MimeMap is more like preferred format of MIME type and Ktor MIME map is fallback, if there's no type in custom MimeMap

--- src/main/kotlin/me/darefox/videosharebot/extensions/HttpResponse.kt
+++ src/main/kotlin/me/darefox/videosharebot/extensions/HttpResponse.kt
@@ -2,6 +2,7 @@ package me.darefox.videosharebot.extensions
 
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import me.darefox.videosharebot.http.MimeMap
 import java.net.URL
 
 fun HttpResponse.filename(): String? {
@@ -10,7 +11,9 @@ fun HttpResponse.filename(): String? {
         regex.find(it)?.value
     }
     val contentType = contentType()?.let { contentType ->
-        val extension = contentType.fileExtensions().firstOrNull() ?: return@let null
+        val extension = MimeMap[contentType.contentType + "/" + contentType.contentSubtype]
+            ?: contentType.fileExtensions().firstOrNull()
+            ?: return@let null
         URL(this.request.url.toString()).toSafeFilename() + ".$extension"
     }
     return contentDispositionResult ?: contentType
