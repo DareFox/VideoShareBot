2e4ecd4d087472b0fb753024434173a8144de9f0
DareFox <47672780+DareFox@users.noreply.github.com>
2024-02-06 15:37:13 +0300

    tooling(maxFileSizeByte): add minimum default size on null guild

--- src/main/kotlin/me/darefox/videosharebot/kord/extensions/Guild.kt
+++ src/main/kotlin/me/darefox/videosharebot/kord/extensions/Guild.kt
@@ -4,8 +4,16 @@ import dev.kord.common.entity.PremiumTier
 import dev.kord.core.entity.Guild
 
 val Guild.maxByteFileSize: Long
-    get() = when (this.premiumTier) {
+    get() = premiumTier.maxByteFileSize()
+
+fun Guild?.maxByteFileSizeOrMin(): Long {
+    return this?.maxByteFileSize ?: PremiumTier.None.maxByteFileSize()
+}
+
+fun PremiumTier.maxByteFileSize(): Long {
+    return when (this) {
         PremiumTier.Two -> 50_000_000
         PremiumTier.Three -> 100_000_000
         else -> 25_000_000
     }
+}
