8f206bd15d686decafb8531c9834f038d2b76c96
DareFox <47672780+DareFox@users.noreply.github.com>
2024-02-07 14:01:05 +0300

    refactor(CobaltResponseFactory): use data class object as argument

---
+++ src/main/kotlin/me/darefox/videosharebot/kord/media/upload/UploadContext.kt
@@ -0,0 +1,13 @@
+package me.darefox.videosharebot.kord.media.upload
+
+import dev.kord.core.entity.Message
+import me.darefox.cobaltik.wrapper.WrappedCobaltResponse
+import me.darefox.videosharebot.kord.extensions.BotMessage
+import me.darefox.videosharebot.kord.extensions.BotMessageStatus
+
+data class UploadContext<T: WrappedCobaltResponse>(
+    val userMessage: Message,
+    val botMessage: BotMessage,
+    val botMessageStatus: BotMessageStatus,
+    val cobaltResponse: T
+)
