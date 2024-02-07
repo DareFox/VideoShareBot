e68cf98bf0e57999b82f769aaf1a0714f094b848
DareFox <easymalink@gmail.com>
2024-02-07 13:09:07 +0300

    refactor(TextRange): rename to TextLengthRange

--- src/main/kotlin/me/darefox/videosharebot/match/segments/TextLengthRange.kt
+++ src/main/kotlin/me/darefox/videosharebot/match/segments/TextLengthRange.kt
@@ -2,6 +2,6 @@ package me.darefox.videosharebot.match.segments
 
 import me.darefox.videosharebot.match.UrlSegmentMatcher
 
-class TextRange(val lengthRange: IntRange): UrlSegmentMatcher {
+class TextLengthRange(val lengthRange: IntRange): UrlSegmentMatcher {
     override fun validate(text: String): Boolean = text.length in lengthRange
 }
