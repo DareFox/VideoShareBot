0c3ce0f6811e233f294e7a6362389ab01b41830f
DareFox <47672780+DareFox@users.noreply.github.com>
2024-02-13 15:49:10 +0300

    tooling(Coroutines): creating child scope and add onCancel callback

---
+++ src/main/kotlin/me/darefox/videosharebot/extensions/CoroutineScope.kt
@@ -0,0 +1,22 @@
+package me.darefox.videosharebot.extensions
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+fun CoroutineScope.createChildScope(useSupervisor: Boolean, context: CoroutineContext? = null): CoroutineScope {
+    val job = when {
+        useSupervisor -> SupervisorJob(parent = this.coroutineContext[Job])
+        else -> Job(parent = this.coroutineContext[Job])
+    }
+    return (context?.let { CoroutineScope(it) } ?: this) + job
+}
+
+fun CoroutineScope.onCancel(context: CoroutineContext? = null, func: (CancellationException) -> Unit) = launch(context ?: EmptyCoroutineContext) {
+    try {
+        awaitCancellation()
+    } catch (ex: CancellationException) {
+        func(ex)
+        throw ex
+    }
+}
