9c06612854e648a3eb913f760dcfb79707dd8b91
DareFox <47672780+DareFox@users.noreply.github.com>
2024-02-18 13:57:10 +0300

    refactor(Uploader): Return error objects instead of strings

--- src/main/kotlin/me/darefox/videosharebot/kord/media/upload/CobaltResponseFactory.kt
+++ src/main/kotlin/me/darefox/videosharebot/kord/media/upload/CobaltResponseFactory.kt
@@ -1,11 +1,14 @@
 package me.darefox.videosharebot.kord.media.upload
 
+import dev.forkhandles.result4k.peekFailure
 import me.darefox.cobaltik.wrapper.PickerResponse
 import me.darefox.cobaltik.wrapper.RedirectResponse
 import me.darefox.cobaltik.wrapper.StreamResponse
 import me.darefox.cobaltik.wrapper.WrappedCobaltResponse
+import me.darefox.videosharebot.extensions.ResultMonad
 import me.darefox.videosharebot.match.CompositeMatcherResult
 import me.darefox.videosharebot.match.services.InstagramMatcher
+import me.darefox.videosharebot.tools.stringtransformers.DoNothingWithString
 
 @Suppress("UNCHECKED_CAST")
 object CobaltResponseFactory {
@@ -14,22 +17,31 @@ object CobaltResponseFactory {
         parsedResult: CompositeMatcherResult,
         eventContext: UploadContext<WrappedCobaltResponse>
     ) {
-        when (eventContext.cobaltResponse) {
+        var result: ResultMonad<Unit, UploadError>? =  when (eventContext.cobaltResponse) {
             is PickerResponse -> PickerUploader.upload(eventContext as UploadContext<PickerResponse>)
             is RedirectResponse -> {
                 val context = eventContext as UploadContext<RedirectResponse>
                 if (parsedResult.parser in alwaysStream) {
                     val asStream = StreamResponse(context.cobaltResponse.redirectUrl)
                     val newContext = eventContext.copy(cobaltResponse = asStream)
-                    this.uploadMedia(parsedResult, newContext)
+                    StreamUploader.upload(newContext as UploadContext<StreamResponse>)
                 } else {
                     RedirectUploader.upload(context)
                 }
             }
             is StreamResponse -> StreamUploader.upload(eventContext as UploadContext<StreamResponse>)
-            else -> eventContext.botMessageStatus.changeTo("${eventContext.cobaltResponse} is not supported")
+            else -> {
+                eventContext.botMessageStatus.changeTo("${eventContext.cobaltResponse} is not supported")
+                null
+            }
+
+        }
+
+        result?.peekFailure {
+            eventContext.botMessageStatus.cancel(
+                content = it.asDiscordMessageText,
+                overrideTransformer = DoNothingWithString
+            )
         }
     }
-
-
 }
