15e421c8d67a56b5eb97402271394a755f216da3
DareFox <47672780+DareFox@users.noreply.github.com>
2024-02-18 13:56:27 +0300

    docs(ResultMonad): Rename generics to more understandable names

--- src/main/kotlin/me/darefox/videosharebot/extensions/Result.kt
+++ src/main/kotlin/me/darefox/videosharebot/extensions/Result.kt
@@ -5,7 +5,7 @@ import dev.forkhandles.result4k.Result
 import dev.forkhandles.result4k.Success
 
 /** Type alias for [Result] to simplify importing it (Kotlin, Ktor and Result4k provide same class name for this monad) */
-typealias ResultMonad<T,E> = Result<T,E>
+typealias ResultMonad<Val,Err> = Result<Val,Err>
 
 /** Return empty [Success] **/
 fun Success() = Success(Unit)
@@ -13,23 +13,23 @@ fun Success() = Success(Unit)
 /**
  * Executes the provided [block] and wraps the result in a [ResultMonad].
  *
- * @param block The code block to be executed, producing a result of type [T].
+ * @param block The code block to be executed, producing a result of type [Val].
  * @return A [ResultMonad] representing the result of the operation.
  *   - If the block execution is successful, returns a [Success] with the result.
- *   - If an exception of type [E] is caught during execution, returns a [Failure] with the caught exception.
+ *   - If an exception of type [Err] is caught during execution, returns a [Failure] with the caught exception.
  *   - If an exception of a different type is caught, rethrows the exception.
  *
- * @param T The type of the result produced by the [block].
- * @param E The type of exception that the function is expected to handle.
+ * @param Val The type of the result produced by the [block].
+ * @param Err The type of exception that the function is expected to handle.
  * @return A [ResultMonad] encapsulating the result or exception.
  *
- * @throws Exception If an exception of a type other than [E] is caught during execution, it is rethrown.
+ * @throws Exception If an exception of a type other than [Err] is caught during execution, it is rethrown.
  */
-inline fun <T, reified E: Exception> tryAsResult(block: () -> T): ResultMonad<T, E> {
+inline fun <Val, reified Err: Exception> tryAsResult(block: () -> Val): ResultMonad<Val, Err> {
     return try {
         Success(block())
     } catch (e: Exception) {
-        if (e is E) {
+        if (e is Err) {
             Failure(e)
         } else {
             throw e
@@ -46,12 +46,12 @@ inline fun <T, reified E: Exception> tryAsResult(block: () -> T): ResultMonad<T,
  *                   The lambda receives a [Failure] instance and returns a result of type [R].
  * @return The result of applying the appropriate transformation based on the [ResultMonad] type.
  *
- * @param T The type of the value encapsulated by [Success].
- * @param F The type of the error encapsulated by [Failure].
+ * @param Val The type of the value encapsulated by [Success].
+ * @param Err The type of the error encapsulated by [Failure].
  * @param R The type of the result produced by the transformation functions.
  * @return The result of applying the transformation functions.
  */
-inline fun <T, F, R> ResultMonad<T, F>.fold(ifSuccess: Success<T>.() -> R, ifFailure: Failure<F>.() -> R): R {
+inline fun <Val, Err, R> ResultMonad<Val, Err>.fold(ifSuccess: Success<Val>.() -> R, ifFailure: Failure<Err>.() -> R): R {
     return when(this) {
         is Failure -> ifFailure(this)
         is Success -> ifSuccess(this)
