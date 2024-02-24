dd80fd8812b0a73175e158f8b842043b26794e7c
DareFox <47672780+DareFox@users.noreply.github.com>
2024-02-24 18:04:54 +0300

    feat(FFmpeg): Implement FFmpeg class

--- src/main/kotlin/me/darefox/videosharebot/ffmpeg/FFmpeg.kt
+++ src/main/kotlin/me/darefox/videosharebot/ffmpeg/FFmpeg.kt
@@ -1,6 +1,118 @@
 package me.darefox.videosharebot.ffmpeg
 
+import dev.forkhandles.result4k.Failure
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.updateAndGet
+import me.darefox.videosharebot.extensions.*
+import raceWithCancellation
+import kotlin.streams.asSequence
+import kotlin.time.Duration.Companion.seconds
+
 class FFmpeg(
     val ffmpegPath: String,
     val arguments: List<String>,
+) {
+    private val log = createLogger()
+    private val fullCommand = getFullCommand("-stats_period 0.5 -progress pipe:1".split(" "))
+    private val processBuilder = ProcessBuilder(fullCommand)
+    private var process: Process? = null
+
+    private val _status = MutableStateFlow<FFmpegStatus>(FFmpegStatus.NotStarted)
+    val status = _status as StateFlow<FFmpegStatus>
+
+    suspend fun process(): ResultMonad<Unit, FFmpegError> = coroutineScope {
+        val processScope = CoroutineScope(Dispatchers.IO)
+        val cancellation = onCancel {
+            log.logCancel(it)
+            killFFmpeg()
+            processScope.cancel()
+        }
+
+        log.info { "Starting ffmpeg with command:\n$fullCommand" }
+        val processRef = withContext(Dispatchers.IO) {
+            processBuilder.start()
+        }
+        process = processRef
+        val errorList = mutableListOf<String>()
+
+        processScope.apply {
+            launch {
+                collectErrorsToList(processRef, errorList)
+            }
+            launch(CoroutineName("StatusConverter")) {
+                convertOutputToStatus(processRef)
+            }
+        }
+
+        val code = withContext(Dispatchers.IO) {
+            processRef.waitFor()
+        }
+        cancellation.cancel()
+
+        when {
+            code != 0 -> {
+                setStatusToError()
+                Failure(FFmpegError(errorList, code))
+            }
+            else -> Success()
+        }
+    }
+
+    private fun setStatusToError() {
+        _status.updateAndGet {
+            when (it) {
+                is FFmpegStatus.Continue -> FFmpegStatus.Error(it.progress)
+                is FFmpegStatus.End -> throw IllegalStateException("You shouldn't get here")
+                is FFmpegStatus.Error -> it
+                FFmpegStatus.NotStarted -> FFmpegStatus.Error(null)
+            }
+        }
+    }
+
+    private suspend fun collectErrorsToList(processRef: Process, errorList: MutableList<String>) {
+        processRef.errorStream.bufferedReader().lines().asSequence().forEach {
+            log.error { "STDERR: $it" }
+            errorList += it
+            yield()
+        }
+    }
+
+    private suspend fun convertOutputToStatus(processRef: Process) {
+        val converter = FFmpegStatusConverter()
+        processRef.inputStream.bufferedReader().lines().asSequence().forEach {
+            log.debug { "STDIN: $it" }
+            val progress = converter.addLine(it)
+            if (progress != null) _status.value = progress
+            yield()
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun killFFmpeg() {
+        GlobalScope.launch {
+            raceWithCancellation(
+                raceOne = {
+                    if (process?.isAlive == false) return@raceWithCancellation
+                    log.debug { "Trying to kill gracefully" }
+                    process?.destroy()
+                    log.debug { "Killed gracefully" }
+                },
+                raceTwo = {
+                    delay(20.seconds)
+                    process?.destroyForcibly()
+                    log.debug { "Killed by force" }
+                }
+            )
+        }
+    }
+
+    private fun getFullCommand(globalOptions: List<String>) = listOf(ffmpegPath) + globalOptions + arguments
+}
+
+data class FFmpegError(
+    val errorLines: List<String>,
+    val exitCode: Int
 )
+
