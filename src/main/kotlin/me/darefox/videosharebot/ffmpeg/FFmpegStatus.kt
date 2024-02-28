103c90f5c785ae8d560bf1902ecac40cc8b8eb10
DareFox <47672780+DareFox@users.noreply.github.com>
2024-02-28 14:18:53 +0300

    refactor(FFmpeg): give error code and lines in FFmpegError status

--- src/main/kotlin/me/darefox/videosharebot/ffmpeg/FFmpegStatus.kt
+++ src/main/kotlin/me/darefox/videosharebot/ffmpeg/FFmpegStatus.kt
@@ -4,5 +4,5 @@ sealed class FFmpegStatus {
     data object NotStarted : FFmpegStatus()
     data class Continue(val progress: FFmpegProgress) : FFmpegStatus()
     data class End(val progress: FFmpegProgress) : FFmpegStatus()
-    data class Error(val progress: FFmpegProgress?) : FFmpegStatus()
+    data class Error(val errorCode: Int, val errorLines: List<String>, val progress: FFmpegProgress?) : FFmpegStatus()
 }
